e7b71b52515c76f25bb95e9f76a752bad0427d3c
GamingPerson11 <belladecat131@gmail.com>
2025-03-12 22:36:24 +0000

    Update Util.bat

--- Util.bat
+++ Util.bat
@@ -1,5 +1,6 @@
 @echo off
 if exist custmcolor.txt (
+	::Credits to ChatGPT for making this one line of code
 	for /f "delims=" %%A in ('type custmcolor.txt') do set custmclrfile=%%A
 ) else (
 	color 9
